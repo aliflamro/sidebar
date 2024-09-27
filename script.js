3bbea8aa6692a4566ca77e40ac8d515828461b01
SUPERBOY <99008727+aliflamro@users.noreply.github.com>
2024-09-27 12:44:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var docs = document.getElementById('sides');
+
+function openNav(isNav){
+    if(isNav == 2){
+        docs.style.display = "block";
+    }else{
+        docs.style.display = "none";
+    }
+}
